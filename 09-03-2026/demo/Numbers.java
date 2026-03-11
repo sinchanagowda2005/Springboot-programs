bcd6d6d4e1852b9ecf56e7f95074f177b78c9ee9
sinchanagowda2005 <knsinchana40@gmail.com>
2026-03-11 10:21:56 +0530

    Spring-boot

---
+++ 09-03-2026/demo/Numbers.java
@@ -0,0 +1,25 @@
+
+    package com.example.demo;
+
+public class Numbers {
+    private int a;
+    private int b;
+
+    // Getters and setters
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+}
+
---
+++ 09-03-2026/demo/Student (1).java
@@ -0,0 +1,33 @@
+
+    
+    package com.example.demo;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class Student {
+    public int m1,m2,m3,m4,m5;
+
+    @PostMapping("/marks")
+    public int total(@RequestBody Student s)
+    {
+        return s.m1+s.m2+s.m3+s.m4+s.m5;
+    }
+
+    @PostMapping("/average")
+    public String average(@RequestBody Student s)
+    {
+        int avg=(s.m1+s.m2+s.m3+s.m4+s.m5)/5;
+
+        if(avg>=75){
+            return "Eligible";
+        }
+       else{
+        return "Not Eligible";
+       }
+    }
+
+
+    
+}
