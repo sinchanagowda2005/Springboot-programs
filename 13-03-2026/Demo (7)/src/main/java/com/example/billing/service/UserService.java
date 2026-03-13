b88579bc994675baea9c70d84d3417326cf1da3f
sinchanagowda2005 <knsinchana40@gmail.com>
2026-03-13 16:23:14 +0530

    Add files via upload

---
+++ 13-03-2026/Demo (7)/src/main/java/com/example/billing/service/UserService.java
@@ -0,0 +1,30 @@
+package com.example.billing.service;
+
+import com.example.billing.entity.UserEntity;
+import com.example.billing.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public UserEntity createUser(UserEntity user) {
+        return userRepository.save(user);
+    }
+
+    public List<UserEntity> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<UserEntity> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+}
